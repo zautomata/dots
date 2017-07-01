5b4267057719f5b87afda91972fc2350ad739dc9
zautomata <zautomata@gmail.com>
2017-07-01 20:03:45 +0200

    update snippets

--- .emacs.d/elpa/julia-mode-20170426.1806/julia-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "julia-mode" "20170426.1806" "Major mode for editing Julia source code" 'nil :commit "46d2469c408888cfeeec27904116cfc22ceb1461" :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
--- .emacs.d/elpa/kooten-theme-20161023.205/kooten-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kooten-theme" "20161023.205" "Dark color theme" '((emacs "24.1")) :commit "d10197b4dd7af02cd14aeab2573c273a294798c3" :url "http://github.com/kootenpv/emacs-kooten-theme" :keywords '("themes"))
